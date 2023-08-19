26236c9dcef416b3541070ab4ade424252154136
NeoDitjoe <ditjoeneo03@gmail.com>
2023-08-19 21:39:28 +0200

    styled the login page

--- src/Auth/LoginIn.jsx
+++ src/Auth/LoginIn.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useStateContext } from "../UseContext/UseContext"
 import supabase from "./supabase"
 import ScrollToTop from "../scrollToTop/ScrollToTop"
+import logo from '../assets/logo.png'
 
 export default function LoginIn(){
 
@@ -14,9 +15,10 @@ export default function LoginIn(){
     }
     
     return (
-        <>
+        <div style={{ display:'flex', flexDirection: 'column', textAlign:'center', marginLeft:"25%"}}>
             <ScrollToTop/>
-            { user ? <button type="button" className="btn btn-danger" onClick={() => supabase.auth.signOut()}>logout</button> : <button type="button" className="btn btn-success" onClick={login}> login with Github</button>}
-        </>   
+            <img src={logo} alt="" style={{ width:'70%'}} />
+            { user ? <button style={{ width:'70%'}} type="button" className="btn btn-danger" onClick={() => supabase.auth.signOut()}>logout</button> : <button style={{ width:'70%'}} type="button" className="btn btn-success" onClick={login}> login with Github</button>}
+        </div>   
     )
 }

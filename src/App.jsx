88bf7a8176de5c89460867e5bc10751d757be97a
NeoDitjoe <ditjoeneo03@gmail.com>
2023-08-19 15:19:33 +0200

    user stays logged in unless logs out

--- src/App.jsx
+++ src/App.jsx
@@ -53,31 +53,19 @@ function App() {
   // const [ audioLayout, setAudioLayout ] = useState(false)
 
   useEffect(() => {
-    const session = supabase.auth.getSession()
-    setUser(session?.user)
-
-    const unsubscribe = () => supabase.auth.onAuthStateChange((event, session) => {
-      switch(event) {
-        case 'SIGNED_IN':
-          setUser(session?.user)
-          
-          /**
-           * store the user id in a usestate then, the state is used to get spicific 
-           * users data and to also store the id to the database
-           */
-          setUserId(session.user.id)
-          
-          break;
-        case 'SIGNED_OUT':
-          setUser(null)
-          break;
-        default:  
-      }
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session)
+      setUserId(session.user.id)
     })
 
-    return () => {
-      unsubscribe()
-    }
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session)
+      setUserId(session.user.id)
+    })
+
+    return () => subscription.unsubscribe()
   }, [])
 
  return (

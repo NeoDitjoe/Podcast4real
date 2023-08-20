d240ea1f91f65cfba2b7eba49beb3bf86b19dc78
NeoDitjoe <ditjoeneo03@gmail.com>
2023-08-20 17:55:31 +0200

    edited error page

--- src/ErrorMessage/ErrorMessage.jsx
+++ src/ErrorMessage/ErrorMessage.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 
 const ErrorMessage = () => {
     return (
@@ -5,6 +6,7 @@ const ErrorMessage = () => {
             <h2 style={{color:"orange"}}>Oops! Something went wrong.</h2>
             <p>There was a problem fetching the data.</p>
             <p>Please check your internet connection and try again.</p>
+            <p>If error continues to persist, go back to the home page <NavLink to='/'>Home</NavLink></p>
             <button style={{color:"white", padding:'1%'}} onClick={() => window.location.reload()}>Refresh Page</button>
       </div>
     );
